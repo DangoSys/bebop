4e5bc01a254f938da4361de018f22f89005ebc1c
shirohasuki <whmio0115@gmail.com>
2025-12-30 13:59:15 +0800

    refactor: consolidate installation scripts by moving to a new install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+BEBOP_DIR=$(git rev-parse --show-toplevel)
+
+cd $BEBOP_DIR
+git submodule update --init
+
+$BEBOP_DIR/host/spike/install-spike.sh
+$BEBOP_DIR/host/gem5/install-gem5.sh
